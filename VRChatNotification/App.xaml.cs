a4ded3eb807374278c03b4e26d424b5e1976a335
hrt05 <hirate05program@gmail.com>
2026-08-09 01:23:28 +0900

    プロジェクトファイルを追加。

---
+++ VRChatNotification/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace VRChatNotification
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ VRChatNotification/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Media;
+using System.Windows;
+
+namespace VRChatNotification
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async Task PlaySound()
+        {
+            // C:\Users\kouki\source\repos\VRChatNotification\VRChatNotification\bin\Debug\net10.0-windows
+            // C:\Users\kouki\source\repos\VRChatNotification\VRChatNotification\bin\Debug\net10.0-windows/sound/joinSound.wav
+
+            var player = new SoundPlayer(Path.Combine(Directory.GetCurrentDirectory(), "sound", "joinSound.wav"));
+            player.Play();
+        }
+
+        private async void Ignition(object sender, RoutedEventArgs e)
+        {
+            //var tetetest = Directory.GetCurrentDirectory();
+            //Debug.WriteLine(tetetest);
+
+            try
+            {
+                await PlaySound();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+    }
+}
